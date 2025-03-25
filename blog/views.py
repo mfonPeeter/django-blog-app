b2595391bc3a69ff52f104bab88e3a26bac24cb2
mfonPeeter <godimfon@gmail.com>
2025-03-25 16:46:43 +0100

    add comment system for blog

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView
+from django.views.decorators.http import require_POST
 from django.core.mail import send_mail
 from .models import Post
-from .forms import EmailPostForm
+from .forms import CommentForm, EmailPostForm
 
 
 class PostListView(ListView):
@@ -40,7 +41,13 @@ def post_detail(request, year, month, day, post):
         publish__year=year,
         publish__month=month,
         publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+
+    # List of active coments for this post
+    comments = post.comments.filter(active=True)
+    # Form for users to comments
+    form = CommentForm()
+
+    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form})
 
 
 def post_share(request, post_id):
@@ -78,4 +85,26 @@ def post_share(request, post_id):
         request,
         'blog/post/share.html',
         {'post': post, 'form': form, 'sent': sent}
+    )
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+
+    comment = None
+
+    # A comment was posted
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # Create a Comment object without saving it to the database
+        comment = form.save(commit=False)
+        # Assign the post to the comment
+        comment.post = post
+        # Save the comment to the database
+        comment.save()
+    return render(
+        request,
+        'blog/post/comment.html',
+        {'post': post, 'form': form, 'comment': comment}
     )
