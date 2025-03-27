7bf5bbfda680409df0aa0b9ec7bce8b367e24d5c
mfonPeeter <godimfon@gmail.com>
2025-03-27 09:02:53 +0100

    add sitemap to the site

--- django_blog_application/settings.py
+++ django_blog_application/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+SITE_ID = 1
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -38,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
     'taggit',
     'blog.apps.BlogConfig',
 ]
