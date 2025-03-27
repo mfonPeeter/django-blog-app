037448ead6158a0b6fe5fe2e58a8a5e537b4b803
mfonPeeter <godimfon@gmail.com>
2025-03-27 07:22:14 +0100

    retrieve posts by similarity

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView
 from django.views.decorators.http import require_POST
 from django.core.mail import send_mail
+from django.db.models import Count
 from taggit.models import Tag
 from .models import Post
 from .forms import CommentForm, EmailPostForm
@@ -54,7 +55,16 @@ def post_detail(request, year, month, day, post):
     # Form for users to comments
     form = CommentForm()
 
-    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form})
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(
+        tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count(
+        'tags')).order_by('-same_tags', '-publish')[:4]
+
+    print(similar_posts)
+
+    return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'form': form, 'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
