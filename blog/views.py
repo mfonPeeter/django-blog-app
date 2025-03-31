ab254affdbae4aef252fecc2fb21d6e05ae88d20
mfonPeeter <godimfon@gmail.com>
2025-03-31 12:29:16 +0100

    add stemming, ranking, weighting and trigram similarity to search

--- blog/views.py
+++ blog/views.py
@@ -6,7 +6,7 @@ from django.views.generic import ListView
 from django.views.decorators.http import require_POST
 from django.core.mail import send_mail
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, TrigramSimilarity
 from taggit.models import Tag
 from .models import Post
 from .forms import CommentForm, EmailPostForm, SearchForm
@@ -137,9 +137,19 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            results = Post.published.annotate(
-                search=SearchVector('title', 'body')
-            ).filter(search=query)
+            search_vector = SearchVector(
+                'title', weight='A') + SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
+            # results = (
+            #     Post.published.annotate(
+            #         search=search_vector,
+            #         rank=SearchRank(search_vector, search_query)
+            #     )
+            #     .filter(search=search_query)
+            #     .order_by('-rank__gte=0.3')
+            # )
+            results = (Post.published.annotate(similarity=TrigramSimilarity(
+                'title', query)).filter(similarity__gt=0.1).order_by('-similarity'))
 
     return render(
         request,
